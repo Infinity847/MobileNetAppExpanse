83a266c8382df6b5ffce01a0b4fbecb157cf7723
Infinity847 <88909782+Infinity847@users.noreply.github.com>
2022-08-08 13:02:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+canvas.center();
+canvas.position(0,500)
+    video = createCapture(VIDEO);
+    video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+function modelLoaded() {
+    console.log('MODEL IS LOADED');
+}
+function draw() {
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult)
+   
+   }
+   var previous = '';
+   function gotResult(error,results) 
+   {
+   if (error) {
+     console.error(error);
+   }else {
+     if ((results[0].confidence > 0.5) && (previous != results[0].label))
+     {
+      previous = results[0].label;
+       console.log(results);
+       document.getElementById("item").innerHTML = "Item : " +results[0].label;
+       document.getElementById("accuracy").innerHTML = "Accuracy : "+results[0].confidence / 1 * 100 + "%";
+     saytext(results[0].label + "found.");
+      }
+   }
+   }
+   function saytext(text) {
+    const sound = new SpeechSynthesisUtterance(text);
+    sound.rate = 1;
+    speechSynthesis.speak(sound);
+}
